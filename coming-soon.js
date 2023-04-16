6a74a0433d7596e4c6973da4b4b4bf7fd6418ef8
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-04-16 05:44:59 +0100

    js email validation

--- coming-soon.js
+++ coming-soon.js
@@ -1,10 +1,11 @@
 const input = document.querySelector('input')
 const button = document.querySelector('button')
 const error = document.querySelector('#errorMessage')
+const errorIcon = document.querySelector('.errorIcon')
 
 button.addEventListener('click', (clickEvent) => {
     clickEvent.preventDefault()
-    
+
     function validEmail(email) {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailPattern.test(email)
@@ -12,11 +13,16 @@ button.addEventListener('click', (clickEvent) => {
 const email = input.value.trim()
 
 if (email === '') {
+    errorIcon.style.display = 'block'
    error.innerHTML = 'Please enter your email address'
     return
 }if (!validEmail(email)) {
+    errorIcon.style.display = 'block'
     error.innerHTML = 'Please enter a valid email address'
     return
+}if (validEmail(email)) {
+    error.innerHTML = 'Submited'
+    errorIcon.style.display = 'none'
 }
 
 })

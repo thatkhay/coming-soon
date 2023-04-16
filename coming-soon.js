3ddad8a644c178d029e4d2f3413f3bd18d09223a
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-04-16 03:51:39 +0100

    added  javascript to the form for email validation

--- coming-soon.js
+++ coming-soon.js
@@ -1,1 +1,26 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const error = document.querySelector('#errorMessage')
+
+button.addEventListener('click', (clickEvent) => {
+    clickEvent.preventDefault()
+    
+    function validEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email)
+    }
+const email = input.value.trim()
+
+if (email === '') {
+   error.innerHTML = 'Please enter your email address'
+    return
+}if (!validEmail(email)) {
+    error.innerHTML = 'Please enter a valid email address'
+    return
+}
+
+})
+
+
+
 
